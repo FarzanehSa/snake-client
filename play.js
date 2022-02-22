6b4f907a897d9934b8bb9a5b494fb44d3977394b
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-02-21 19:05:12 -0800

    implement play.jswith first connection setup and event handler(data)

---
+++ play.js
@@ -0,0 +1,28 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',// or IP address
+    port: 50541 // PORT number
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+/*   // handle first established connection and prints a message
+  conn.on("connect", () => {
+    console.log('Connect Successfully')
+  }); */
+  
+  // handle incoming data and console.log it for the player.
+  conn.on("data", (data) => {
+    console.log('Message from Game Server:   ',data)
+  });
+
+  return conn;
+};
+
+
+console.log("Connecting ...");
+connect();
