55933eca72eff6e06db35a7034561eb8590f0780
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-02-21 23:02:42 -0800

    implement setupInput and event handler for when data received from stdin

--- client.js
+++ client.js
@@ -11,16 +11,29 @@ const connect = function () {
   conn.setEncoding("utf8");
 
   // handle first established connection and prints a message
+  // give a name to snake
   conn.on("connect", () => {
     console.log('Successfully connected to game server');
     conn.write('Name: FA7')
   });
   
+/*   // handle connection and move up as soon as connection stablished
+  conn.on("connect", () => {
+    conn.write('Move: up')
+    setTimeout(() => {
+      conn.write('Move: right')
+    },50)
+    // setInterval(() => {
+    //   conn.write('Move: up')
+    // },50)
+  }); */
+
   // handle incoming data and console.log it for the player.
   conn.on("data", (data) => {
     console.log('Message from Game Server:   ',data)
   });
 
+  // returns a conn object that allowes us to interact with the server.
   return conn;
 };
 
