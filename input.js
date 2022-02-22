cb5b754714f30c4d8ab0457ae1cf0324b04b8a72
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-02-22 07:50:21 -0800

    pass the conn object returned by connect() to the setUpInput function and setup movement keys

--- input.js
+++ input.js
@@ -1,12 +1,18 @@
+// Stores the active TCP connection object.
+let connection;
+
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = function (conn) {
+
+  connection = conn; // conn is a reference to the object, returned by connect()
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   
   stdin.on("data", handleUserInput);
-  
+
   // returns stdin object allows us to listen for keyboard input and react to it.
   return stdin;
 };
@@ -16,6 +22,27 @@ const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
   }
+  // key to move up
+  if (key === 'w') {
+    // console.log('Move: up');
+    connection.write('Move: up')
+  }
+  // key to move left
+  if (key === 'a') {
+    // console.log('Move: left');
+    connection.write('Move: left');
+  }
+  // key to move down
+  if (key === 's') {
+    // console.log('Move: down');
+    connection.write('Move: down');
+  }
+  // key to move right
+  if (key === 'd') {
+    // console.log('Move: right');
+    connection.write('Move: right');
+  }
+  
 };
 
 module.exports = {setupInput};
--- play.js
+++ play.js
@@ -2,7 +2,8 @@ const {connect} = require('./client');
 const {setupInput} = require('./input');
 
 console.log("Connecting ...");
-connect();
 
-setupInput();
+// pass the object returned from connect into the setupInput function.
+const connection =  connect();
+setupInput(connection);
 
